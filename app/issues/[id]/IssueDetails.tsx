8246347c6adbf2a77c8debea5db1bdd765de9fd3
vish448 <vishangsoni@gmail.com>
2024-07-22 23:36:49 -0400

    refactor: apply the single reposibility principle

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import { IssueStatusBadge } from "@/app/components";
+import { Issue } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import ReactMarkdown from "react-markdown";
+
+const IssueDetails = ({ issue }: {issue:Issue} ) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="5" className="mt-5">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="mt-5 prose">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,48 +1,33 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
-import ReactMarkdown from 'react-markdown'
-import Link  from 'next/link'
-import delay from 'delay'
-import { Pencil2Icon } from '@radix-ui/react-icons'
+import prisma from "@/prisma/client";
+import { Box, Grid } from "@radix-ui/themes";
+import delay from "delay";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
-    params: { id: string}
+  params: { id: string };
 }
 
-const IssueDetailPage = async ({params}: Props) => {
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    })
+const IssueDetailPage = async ({ params }: Props) => {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-    if(!issue)
-        notFound()
+  if (!issue) notFound();
 
-    delay(2000)
+  delay(2000);
 
   return (
-    <Grid columns={{initial:"1",md:"2"}} gap="5">
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="5" className='mt-5'>
-      <IssueStatusBadge status={issue.status} />
-      <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className='mt-5 prose'>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
+        <IssueDetails issue={issue} />.
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issue.id}/edit`}>
-            Edit Issue
-          </Link>
-            </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
