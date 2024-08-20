7667b3972d3931e71f8e45422c697ec641f6c0b7
vish448 <vishangsoni@gmail.com>
2024-08-19 22:56:41 -0400

    Pagination layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
    <div>
     Home Page
+    <Pagination itemCount={100} pageSize={10} currentPage={2}  />
    </div>
   );
 }
