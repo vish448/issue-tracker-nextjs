30a2591da5968d37eb0e756b04040d684354ca72
vish448 <vishangsoni@gmail.com>
2024-08-20 22:35:00 -0400

    layout dashboards

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
 import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home({
   searchParams,
@@ -25,10 +26,13 @@ export default async function Home({
     },
   });
   return (
-    <>
+    <Grid columns={{initial:"1", md:"2"}} gap="5">
+      <Flex direction="column" gap="5">
+
       <IssueSummary open={openIssues} inProgress={inProgressIssues} closed={closedIssues} />
       <IssueChart open={openIssues} inProgress={inProgressIssues} closed={closedIssues} />
+      </Flex>
       <LatestIssue />
-    </>
+    </Grid>
   );
 }
