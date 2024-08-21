10ab1b8c96efd5c51fbfadf0c223adbefd8a040f
vishang soni <vishangsoni@gmail.com>
2024-08-20 21:51:54 -0400

    Build Issue Summary Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatestIssue from "./LatestIssue";
+import IssueSummary from "./IssueSummary";
 
 export default function Home({searchParams}:{searchParams:{page: string}}) {
   return (
+    <>
+    <IssueSummary open={0} inProgress={0} closed={0}  />
    <LatestIssue />
+    </>
   );
 }
